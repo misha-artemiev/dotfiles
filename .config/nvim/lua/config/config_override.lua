1a200464eb0cb31e203b30300f861ba8ef4813c9
Misha Artemiev <mishaartemiev@Mishas-MacBook-Pro.local>
2025-10-26 19:26:41 +0000

    u

---
+++ .config/nvim/lua/config/config_override.lua
@@ -0,0 +1,33 @@
+vim.g.lang_python = true
+vim.g.lang_go = true
+vim.g.lang_java = true
+vim.g.lang_make = true
+vim.g.lang_swift = true
+vim.g.lang_lua = true
+vim.g.lang_just = true
+vim.g.lang_haskell = true
+
+vim.g.plugin_nvim_tree = false
+vim.g.plugin_dashboard = true
+vim.g.plugin_which_key = true
+vim.g.plugin_trouble = true
+vim.g.plugin_noice = true
+vim.g.plugin_lazydev = true
+vim.g.plugin_gitsigns = true
+vim.g.plugin_stay_centered = true
+vim.g.plugin_fzf_lua = true
+vim.g.plugin_lspconfig = true
+vim.g.plugin_mason = true
+vim.g.plugin_blink_cmp = true
+vim.g.plugin_treesitter = true
+vim.g.plugin_toggleterm = true
+vim.g.plugin_mini_indentscope = true
+vim.g.plugin_lualine = true
+vim.g.plugin_flash = true
+vim.g.plugin_transparent = true
+vim.g.plugin_oil = true
+
+vim.g.extension_toggleterm_lazygit = true
+
+vim.g.config_signcolumn = true
+
