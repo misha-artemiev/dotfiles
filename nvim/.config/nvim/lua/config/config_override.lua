5c465752d0aabdf10b8ed8e22381832db050e337
Misha Artemiev <mykhailo.artemiev@icloud.com>
2025-12-19 13:40:09 +0000

    auto-snapshot:  {1 file changed, 4 insertions(+), 4 deletions(-)}

--- nvim/.config/nvim/lua/config/config_override.lua
+++ nvim/.config/nvim/lua/config/config_override.lua
@@ -3,8 +3,8 @@
 --vim.g.lang_java = true
 --vim.g.lang_make = true
 --vim.g.lang_swift = true
---vim.g.lang_lua = true
---vim.g.lang_just = true
+vim.g.lang_lua = true
+vim.g.lang_just = true
 --vim.g.lang_haskell = true
 
 vim.g.plugin_dashboard = true
@@ -15,8 +15,8 @@ vim.g.plugin_lazydev = true
 vim.g.plugin_gitsigns = true
 vim.g.plugin_stay_centered = true
 vim.g.plugin_fzf_lua = true
---vim.g.plugin_lspconfig = true
---vim.g.plugin_mason = true
+vim.g.plugin_lspconfig = true
+vim.g.plugin_mason = true
 vim.g.plugin_blink_cmp = true
 vim.g.plugin_treesitter = true
 vim.g.plugin_toggleterm = true
